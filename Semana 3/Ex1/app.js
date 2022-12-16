0d7c8d689206fb8ceb64eee70ecf112f4abbdef5
Renan Pinho <renan_aguillarpinho@hotmail.com>
2022-12-16 19:24:04 -0300

    Ajuste no Código do Exercicio 1

--- Semana 3/Ex1/app.js
+++ Semana 3/Ex1/app.js
@@ -1,15 +1,20 @@
 let max, min;
 
-let procuraMinMax = () => {
-    let arr = [1, -1];
+let procuraMinMax = (arr) => {
     max = Math.max.apply(null, arr);
     min = Math.min.apply(null, arr);
 
     if(arr == null || arr == undefined || arr == ""){
-        console.log("Não foi possível encontra.");
+        console.log("Não foi possível encontra.\n");
     } else {
-        console.log("Maior valor: " + max);
-        console.log("Menor valor: " + min);
+        console.log("Maior Valor: " + max + "\nMenor Valor: " + min + "\n");
     }
 };
-procuraMinMax();
+
+procuraMinMax([56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47]);
+procuraMinMax([]);
+procuraMinMax([1]);
+procuraMinMax([1, -1]);
+procuraMinMax(null);
+procuraMinMax([-2, -2, -2, -2]);
+procuraMinMax([20, 10, 30]);
